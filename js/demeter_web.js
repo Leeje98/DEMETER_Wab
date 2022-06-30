aa466095efac138a6a1f0464a3aceb1444ed9b65
JE <wjd7dms7@naver.com>
2022-07-01 01:03:52 +0900

    0701_01

--- js/demeter_web.js
+++ js/demeter_web.js
@@ -4,12 +4,15 @@
 const searchBox = document.querySelector('#search')
 
 searchBox.addEventListener('focus', () => {
-    searchBox.setAttribute('width', '  200px')
-            // Element.setAttribute()  지정된 요소의 속성 값을 설정합니다. 속성이 이미 있는 경우 값이 업데이트됩니다. 그렇지 않으면 지정된 이름과 값으로 새 속성이 추가됩니다.
+    searchBox.setAttribute('width', '200px')
+    searchBox.setAttribute('placeholder', '검색어를 입력해주세요')
+            // Element.setAttribute()  지정된 요소의 속성 값을 설정. 속성이 이미 있는 경우 값 업데이트
+            // 그렇지 않으면 지정된 이름과 값으로 새 속성 추가
 })
 
 searchBox.addEventListener('blur', () => {
     searchBox.setAttribute('placeholder', '')
+    searchBox.setAttribute('value', '')
 })
 
 
@@ -17,14 +20,13 @@ searchBox.addEventListener('blur', () => {
 
 $(function () {
 
-
-    $('.lnb01').mouseover(function () {
-        $(".list01").css("height", "500px");
+    $('.lnb').mouseover(function () {
+        $(".Menu_list").css("height", "200px");
     });
 
-    $('.lnb01').mouseleave(function () {
-        $(".list01").css("height", "0px");
+    $('.lnb').mouseleave(function () {
+        $(".Menu_list").css("height", "0px");
     });
 
 
-}) // 01
+}) // lnb
